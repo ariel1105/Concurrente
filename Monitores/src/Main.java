efd607ac9af5c01fc335797d4948978cf698f18e
ariel1105 <73146826+ariel1105@users.noreply.github.com>
2022-11-05 09:57:23 -0300

    Update Main.java

--- Monitores/src/Main.java
+++ Monitores/src/Main.java
@@ -51,28 +51,14 @@ public class Main {
                         throw new RuntimeException(e);
                     }
                 }
+                this.pool.stop();
             }
         }
-        class PoolStopper extends Thread{
-            ThreadPool pool;
-            public PoolStopper(ThreadPool pool){
-                this.pool = pool;
-            }
-
-            public void run(){
-                try {
-                    pool.stop();
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
-                }
-            }
-        }
-
+        
         ThreadPool pool = new ThreadPool(5, 8, 20);
         UserPool user = new UserPool(pool);
-        PoolStopper stopper = new PoolStopper(pool);
         user.start();
-        stopper.start();
+
 
     }
 }
