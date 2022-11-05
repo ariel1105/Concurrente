4068bb4e1b17bdfb918be52bfe59b14d87c9c656
ariel1105 <73146826+ariel1105@users.noreply.github.com>
2022-11-05 09:56:14 -0300

    Update ThreadPool.java

--- Monitores/src/ThreadPool/ThreadPool.java
+++ Monitores/src/ThreadPool/ThreadPool.java
@@ -4,11 +4,6 @@ import src.Buffer.Buffer;
 
 public class ThreadPool {
 
-    private int tasks;
-    private int workers;
-
-    private int working = 0;
-
     private Buffer buffer;
 
     public ThreadPool(int capacidad, int workers, int tasks){
@@ -20,40 +15,16 @@ public class ThreadPool {
             worker.start();
         }
     }
-    public synchronized void beginWork(){
-        this.working++;
-    }
-    public synchronized void finishWork(){
-        this.working--;
-        this.tasks--;
-        System.out.println("tasks: "+ this.tasks);
-        notify();
-    }
-    public Boolean isDone(){
-        return this.tasks == 0;
-    }
-
-    public Boolean idleExists(){
-        return this.working < this.workers;
-    }
 
     public synchronized void launch() throws InterruptedException {
-        while(!idleExists()){
-            wait();
-        }
         DummyTask task = new DummyTask();
         this.buffer.write(task);
-        notify();
     }
 
-    public synchronized void stop() throws InterruptedException {
+    public synchronized void stop() throws RuntimeException {
         PoisonPill poison = new PoisonPill();
-        while(!(isDone())){
-            wait();
-        }
         for(int i = 0; i < this.workers; i++){
             this.buffer.write(poison);
-            notify();
         }
     }
 }
